011cbb4dfd5b3e2dd3dd36e19fdeab18eb172612
meesterstijn <meesterstijnvdwulp@gmail.com>
2026-05-08 23:46:12 +0200

    Updates

--- assets/SpellingTool-DtL9gs3F.js
+++ assets/SpellingTool-DtL9gs3F.js
@@ -1,4 +1,4 @@
-import{a as J,r as o,j as e,S as k,X as M,P as O,T as W}from"./index-RcSjjfGP.js";/**
+import{a as J,r as o,j as e,S as k,X as M,P as O,T as W}from"./index-Da6CXzWM.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
