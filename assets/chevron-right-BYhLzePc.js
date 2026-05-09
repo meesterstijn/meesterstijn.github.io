f5614ce7a64f1078c298db5862d63bcadd124ec1
meesterstijn <meesterstijnvdwulp@gmail.com>
2026-05-09 15:30:47 +0200

    Updates

--- assets/chevron-right-BYhLzePc.js
+++ assets/chevron-right-BYhLzePc.js
@@ -1,4 +1,4 @@
-import{b as t}from"./index-CMbZ4AWK.js";/**
+import{d as t}from"./index-Bk7zKlrU.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
