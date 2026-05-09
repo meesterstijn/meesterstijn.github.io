51c50c495ba2aad3d3a518f29463cab61f050fd1
meesterstijn <meesterstijnvdwulp@gmail.com>
2026-05-09 14:43:43 +0200

    Updates

--- assets/chevron-right-mFN589PG.js
+++ assets/chevron-right-mFN589PG.js
@@ -1,4 +1,4 @@
-import{b as t}from"./index-BwMQsu0K.js";/**
+import{b as t}from"./index-D6MKNo4Y.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
