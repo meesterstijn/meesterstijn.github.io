9137e8afb079f3bf5480e9d1c88800c82b67279d
meesterstijn <meesterstijnvdwulp@gmail.com>
2026-05-09 15:24:12 +0200

    Updates

--- assets/chevron-right-BLWaFlDV.js
+++ assets/chevron-right-BLWaFlDV.js
@@ -1,4 +1,4 @@
-import{b as t}from"./index-DXmGkbQ9.js";/**
+import{b as t}from"./index-CMbZ4AWK.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
