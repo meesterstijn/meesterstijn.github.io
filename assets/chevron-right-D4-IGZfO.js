fc824935f88122086955172f0a2b9be15e7e022a
meesterstijn <meesterstijnvdwulp@gmail.com>
2026-05-09 14:20:55 +0200

    Updates

--- assets/chevron-right-D4-IGZfO.js
+++ assets/chevron-right-D4-IGZfO.js
@@ -1,4 +1,4 @@
-import{b as t}from"./index-DuvRe3h9.js";/**
+import{b as t}from"./index-hPwdkT-s.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
