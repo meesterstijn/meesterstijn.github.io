9a1f49a1300f93531ffbcbc74130453db11ee10d
meesterstijn <meesterstijnvdwulp@gmail.com>
2026-05-11 22:27:55 +0200

    Updates

--- assets/Prive-SXp7_3pj.js
+++ assets/Prive-SXp7_3pj.js
@@ -1,4 +1,4 @@
-import{j as e}from"./vendor-query-D1vOaZhx.js";import{r as o,L as P}from"./vendor-react-BuzZ45xS.js";import{s as y}from"./index-C06imWvC.js";import{S as A,k as H,i as ee,X as B,a1 as te,q as ne,r as re,s as ae,L as oe}from"./vendor-ui-Cf9XuIHF.js";import"./vendor-supabase-CMSt9Qx4.js";const se="nietvoorleerlingen",K="prive_ok",$=`
+import{j as e}from"./vendor-query-D1vOaZhx.js";import{r as o,L as P}from"./vendor-react-BuzZ45xS.js";import{s as y}from"./index-ChR344xa.js";import{S as A,k as H,i as ee,X as B,$ as te,r as ne,s as re,t as ae,L as oe}from"./vendor-ui-b2g9hRwA.js";import"./vendor-supabase-CMSt9Qx4.js";const se="nietvoorleerlingen",K="prive_ok",$=`
   @keyframes blob {
     0%   { transform: translate(0,0) scale(1); }
     33%  { transform: translate(40px,-30px) scale(1.08); }
